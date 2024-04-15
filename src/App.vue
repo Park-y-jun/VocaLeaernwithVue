452d374e0012c98a841bb33988e74aa730f7ad83
smith <aptl741@gmail.com>
2024-04-15 14:57:03 +0900

    폴더구조, 파일배치

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Nav">조건부 렌더링 네비게이션1</div>
+  <div class="Nav">조건부 렌더링 네비게이션2</div>
+  
+  <div class="Route">메인페이지</div>
+  <div class="Route">회원가입</div>
+  <div class="Route">문제풀기</div>      
+  
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+div {
+  border: 1px solid black;
+  text-align: center;
+}
+</style>
