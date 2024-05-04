85243a8dbb4ba2b11720f41371e643a6b66fe46d
smith <aptl741@gmail.com>
2024-05-05 00:54:04 +0900

    에러 핸들링 상태코드 변경

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,4 +1,3 @@
-// errorHandler.js
 export function handleAxiosError(error) {
   if (error.response) {
     const statusCode = error.response.status;
@@ -6,14 +5,17 @@ export function handleAxiosError(error) {
       case 400:
         alert("입력 정보를 다시 확인해 주세요.");
         break;
-      case 409:
-        alert("이미 등록된 이메일이나 사용자 이름입니다.");
+      case 401:
+        alert("인증에 실패했습니다.");
+        break;
+      case 404:
+        alert("요청한 리소스가 서버에서 찾을 수 없습니다.");
         break;
       case 500:
         alert("서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.");
         break;
       default:
-        alert("알 수 없는 오류가 발생했습니다.");
+        alert("서버 오류가 발생했습니다. 나중에 다시 시도해 주세요.");
     }
   } else if (error.request) {
     alert("서버로부터 응답을 받지 못했습니다. 네트워크 연결을 확인해 주세요.");
